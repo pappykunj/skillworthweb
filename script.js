bbe384128b4574ef4f7d68fd78efa4eac4711a72
lilmaverick <anand4pillai@gmail.com>
2023-01-27 23:10:00 +0530

    done for the day

--- script.js
+++ script.js
@@ -63,7 +63,6 @@ const header = document.querySelector("header");
 const sectionOne = document.querySelector("intro");
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".slide-in");
-
 const sectionOneOptions = {
   rootMargin: "-200px 0px 0px 0px"
 };
